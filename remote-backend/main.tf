b690ef25de5a5808582baa355ef8d3f86136a757
Oussama Slimani <slimani.oussama@enis.tn>
2024-11-21 22:19:24 +0200

    first commit

---
+++ remote-backend/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0" # Specify a version constraint
+    }
+  }
+
+  backend "s3" {
+    bucket         = "custom-terraform-state-bucket-123-eee2199c" # Replace with your S3 bucket name
+    key            = "aws-backend/terraform.tfstate"             # Location of the state file in the bucket
+    region         = "us-east-1"                                 # AWS region
+    dynamodb_table = "custom-terraform-state-locks-123"          # Replace with your DynamoDB table name
+    encrypt        = true                                        # Enables encryption for the state file
+  }
+}
+
+
+# AWS provider configuration
+provider "aws" {
+  region = "us-east-1"
+}
+
+# S3 bucket for storing Terraform state
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "${var.s3_bucket_name}-${random_id.bucket_suffix.hex}"
+  force_destroy = true
+}
+
+# Enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption for the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ remote-backend/terraform.tfvars
@@ -0,0 +1,3 @@
+# Optionally, you can override the default values
+s3_bucket_name      = "custom-terraform-state-bucket-123" # It should be unique
+dynamodb_table_name = "custom-terraform-state-locks-123"  # It should be unique
